0c37124904a7cc749628d63ebb9311de34cd3d90
lyemoml <coderfee@outlook.com>
2019-11-29 17:55:00 +0800

    Support create post

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { outputFile } from "fs-extra";
+import { resolve } from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.Gatsby",
+    async () => {
+      const {
+        showInputBox,
+        showInformationMessage,
+        showErrorMessage
+      } = vscode.window;
+      const { postsDir, frontmatter } = getConfiguration();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gatsby-new-post" is now active!');
+      let filename = await showInputBox({
+        placeHolder: "Filename of post, like: how-to-use-gatsby"
+      });
+      if (!filename) {
+        return showInformationMessage("Canceled");
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      const { workspaceFolders } = vscode.workspace;
+      if (!workspaceFolders) {
+        return showErrorMessage("No folder opened");
+      }
+      const { fsPath: rootPath } = workspaceFolders[0].uri;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+      filename = filename.replace(/\?\/\\&\s/gi, "");
+      const filePath = resolve(rootPath, postsDir, filename + ".md");
 
-	context.subscriptions.push(disposable);
+      outputFile(
+        filePath,
+        fileContent(frontmatter),
+        { encoding: "utf-8" },
+        err => {
+          if (err) {
+            return showErrorMessage(err.message);
+          }
+          showInformationMessage(`${filePath} created!`);
+        }
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getConfiguration() {
+  const cfg = vscode.workspace.getConfiguration("gatsby");
+  const postsDir = cfg.get("postsDir") as string;
+  const frontmatter = cfg.get("frontmatter") as [];
+
+  return { postsDir, frontmatter };
+}
+
+function fileContent(frontmatter: string[]) {
+  return ["---", ...frontmatter, "---"]
+    .map(item => (item !== "---" ? `${item}: ''` : `${item}`))
+    .join("\n");
+}
